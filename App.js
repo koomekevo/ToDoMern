e2ec16f8d2ac80e1ff14f113bde232164849ad89
koomekevo <koome.kevo@gurucrafts.net>
2024-04-16 13:32:12 +0300

    Re-modify App.js

--- App.js
+++ App.js
@@ -1,28 +1,13 @@
 import React from "react";
 import { Provider as PaperProvider } from "react-native-paper";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import HomeScreen from "./screens/HomeScreen";
-import AddTaskScreen from "./screens/AddTaskScreen";
-
-const Stack = createStackNavigator();
+import AppNavigator from "./navigation/AppNavigator";
 
 export default function App() {
   return (
     <PaperProvider>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen
-            name="Home"
-            component={HomeScreen}
-            options={{ title: "ToDoMern" }}
-          />
-          <Stack.Screen
-            name="AddTask"
-            component={AddTaskScreen}
-            options={{ title: "Add Task" }}
-          />
-        </Stack.Navigator>
+        <AppNavigator />
       </NavigationContainer>
     </PaperProvider>
   );
