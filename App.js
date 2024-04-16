23e0ca1fbdab87738697e0e23f17ac3decdcba54
koomekevo <koome.kevo@gurucrafts.net>
2024-04-16 13:19:53 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import { Provider as PaperProvider } from 'react-native-paper';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import ToDoListScreen from './screens/ToDoListScreen';
-import AddToDoScreen from './screens/AddToDoScreen';
-import EditToDoScreen from './screens/EditToDoScreen';
+import React from "react";
+import { Provider as PaperProvider } from "react-native-paper";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "./screens/HomeScreen";
+import AddTaskScreen from "./screens/AddTaskScreen";
 
 const Stack = createStackNavigator();
 
@@ -12,10 +11,17 @@ export default function App() {
   return (
     <PaperProvider>
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="ToDoList" component={ToDoListScreen} />
-          <Stack.Screen name="AddToDo" component={AddToDoScreen} />
-          <Stack.Screen name="EditToDo" component={EditToDoScreen} />
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ title: "ToDoMern" }}
+          />
+          <Stack.Screen
+            name="AddTask"
+            component={AddTaskScreen}
+            options={{ title: "Add Task" }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </PaperProvider>
